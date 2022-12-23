8e57c0f3e5f19d9fbeb34daa3c8967e7fe86c384
Deepak kumar <dk815617@gmail.com>
2022-12-23 09:58:31 +0530

    switch cases

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+  // search box func
+  menuType: string = 'default';
+  constructor(private route: Router) {}
+  ngOnInit(): void {
+    this.route.events.subscribe((val: any) => {
+      if (val.url) {
+        console.warn(val.url);
+        if (localStorage.getItem('seller') && val.url.includes('seller')) {
+          console.warn('in seller area');
+          this.menuType = 'seller';
+        } else {
+          this.menuType = 'default';
+          console.warn('outside seller area');
+        }
+      }
+    });
+  }
 }
