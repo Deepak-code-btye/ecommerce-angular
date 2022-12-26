91d3acb88647e9f0b7c1c69b3fdf508fbbe91050
Deepak kumar <dk815617@gmail.com>
2022-12-26 15:42:26 +0530

    api and localstorage save data

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -43,4 +43,16 @@ export class ProductService implements OnInit {
   }
   //  search component
   // searchData() {}
+
+  localAddToCart(data: product) {
+    let cartData = [];
+    let localCart = localStorage.getItem('localCart');
+    if (!localCart) {
+      localStorage.setItem('localCart', JSON.stringify([data]));
+    } else {
+      cartData = JSON.parse(localCart);
+      cartData.push(data);
+      localStorage.setItem('localCart', JSON.stringify(cartData));
+    }
+  }
 }
